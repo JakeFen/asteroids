b7867f75a2bc7824cf06e4dcc93881c4d147e80c
JakeFen <jacobfenwick111@gmail.com>
2024-11-05 17:09:16 -0800

    Added player sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 # 1. Check for player inputs
@@ -9,7 +10,11 @@ from constants import *
 
 def main():
     pygame.init()
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
 
         # Check if user clicks close button
@@ -18,7 +23,11 @@ def main():
                 return
 
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
+        # Limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 # Only called when this file is run directly.
