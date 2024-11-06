458ee4ec3e25f176d780e39336a077d1e6fc71e2
JakeFen <jacobfenwick111@gmail.com>
2024-11-05 23:19:35 -0800

    player movement

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ from player import *
 
 def main():
     pygame.init()
-    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -22,8 +29,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        for obj in updatable:
+            obj.update(dt)
+
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # Limit framerate to 60 FPS
